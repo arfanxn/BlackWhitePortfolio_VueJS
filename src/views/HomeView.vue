7902393aade3a1bbf4b25ea1eca14a3bb213b1b1
Muhammad Arfan <arfan2173@gmail.com>
2025-04-06 11:45:51 +0700

    installed font awesome icons

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
 import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
+    <FontAwesomeIcon :icon="faArrowRight" />
     <h1 class="text-amber-400">HELLO WORLD</h1>
     <TheWelcome />
   </main>
