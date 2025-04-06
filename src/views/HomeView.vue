a46b36790b2e5ead54daead27071e45c4cf935d8
Muhammad Arfan <arfan2173@gmail.com>
2025-04-06 11:17:47 +0700

    initial commit, also installed tailwind

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <h1 class="text-amber-400">HELLO WORLD</h1>
+    <TheWelcome />
+  </main>
+</template>
