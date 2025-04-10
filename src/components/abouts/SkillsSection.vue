faaba4b8aa47e182128d79bd8a352b91819f317f
Muhammad Arfan <arfan2173@gmail.com>
2025-04-10 11:35:22 +0700

    use SkillItem.vue component on SkilsSection.vue

--- src/components/abouts/SkillsSection.vue
+++ src/components/abouts/SkillsSection.vue
@@ -12,20 +12,18 @@
       </h3>
 
       <ul class="flex flex-row flex-wrap items-baseline w-full gap-4 list-none">
-        <li v-for="skill in category.skills" :key="skill.name" class="group relative">
-          <component :is="skill.icon" :class="`text-4xl text-neutral-300 ${skill.iconClass}`" />
-          <!-- Tooltip with pseudo-element arrow -->
-          <div
-            class="absolute -top-8 left-1/2 -translate-x-1/2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 px-2 py-1 bg-neutral-300 rounded-md font-firacode text-xs text-black whitespace-nowrap before:absolute before:bottom-[-4px] before:left-1/2 before:-translate-x-1/2 before:w-2 before:h-2 before:bg-neutral-300 before:rotate-45 before:content-['']"
-          >
-            {{ skill.name }}
-          </div>
-        </li>
+        <SkillItem
+          v-for="skill in category.skills"
+          :key="skill.name"
+          :skill="skill"
+          icon-class="text-4xl"
+        />
       </ul>
     </div>
   </section>
 </template>
 
 <script setup lang="ts">
+import SkillItem from '@/components/SkillItem.vue'
 import { skillCategories } from '@/constants/skillConstants'
 </script>
