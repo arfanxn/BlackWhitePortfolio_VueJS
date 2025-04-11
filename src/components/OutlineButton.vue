30c3826b5abf76443aa4b16d4c89bf7e9ccd6a48
Muhammad Arfan <arfan2173@gmail.com>
2025-04-11 13:11:09 +0700

    created OutlineButton.vue component

---
+++ src/components/OutlineButton.vue
@@ -0,0 +1,9 @@
+<template>
+  <button class="font-firacode text-neutral-300 py-2 px-4 rounded-md outline outline-neutral-300">
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+//
+</script>
