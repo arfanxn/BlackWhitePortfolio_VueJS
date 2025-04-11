cd7e010ebdc765d5f184bf54bccedbb786400a64
Muhammad Arfan <arfan2173@gmail.com>
2025-04-11 13:10:15 +0700

    updated animation duration

--- src/components/SkillItem.vue
+++ src/components/SkillItem.vue
@@ -8,7 +8,7 @@
     />
     <!-- Tooltip with pseudo-element arrow -->
     <div
-      :class="`absolute -top-8 left-1/2 -translate-x-1/2 opacity-0 group-hover/skill:opacity-100 transition-opacity duration-2000 px-2 py-1 bg-neutral-300 rounded-md font-firacode text-xs text-black whitespace-nowrap
+      :class="`absolute -top-8 left-1/2 -translate-x-1/2 opacity-0 group-hover/skill:opacity-100 transition-opacity duration-300 px-2 py-1 bg-neutral-300 rounded-md font-firacode text-xs text-black whitespace-nowrap
       before:absolute before:bottom-[-4px] before:left-1/2 before:-translate-x-1/2 before:w-2 before:h-2 before:bg-neutral-300 before:rotate-45 before:content-['']
       ${isHovered ? 'block' : 'hidden'} ${tooltipClass}`"
     >
