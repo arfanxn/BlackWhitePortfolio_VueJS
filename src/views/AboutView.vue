7fab78badac78d9f99c8e5f8aaab01c6acb9bb1a
Muhammad Arfan <arfan2173@gmail.com>
2025-04-12 21:04:13 +0700

    renamed MainSection into HeroSection

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout>
     <main class="flex flex-col gap-y-14 md:gap-y-16">
-      <MainSection />
+      <HeroSection />
 
       <AboutMeSection />
 
@@ -16,7 +16,7 @@
 
 <script setup lang="ts">
 import Layout from '@/layouts/Layout.vue'
-import MainSection from '@/components/abouts/MainSection.vue'
+import HeroSection from '@/components/abouts/HeroSection.vue'
 import AboutMeSection from '@/components/abouts/AboutMeSection.vue'
 import ExperienceSection from '@/components/abouts/ExperienceSection.vue'
 import EducationSection from '@/components/abouts/EducationSection.vue'
