6e6e570b789859aff1f52ba4fb748c3611567568
Muhammad Arfan <arfan2173@gmail.com>
2025-04-14 18:45:49 +0700

    added scroll lock

--- src/components/AModal.vue
+++ src/components/AModal.vue
@@ -3,11 +3,11 @@
     <div
       v-if="show"
       role="dialog"
-      class="fixed inset-0 z-50 flex items-center justify-center bg-black/50"
+      class="fixed inset-0 z-50 flex items-center justify-center bg-black/50 overflow-hidden"
       @click.self="close"
     >
       <div
-        class="main-container max-h-full main-horizontal-padding main-vertical-padding space-y-2"
+        class="main-container main-horizontal-padding main-vertical-padding space-y-2 overflow-hidden"
       >
         <div class="flex flex-row justify-end">
           <button @click="close" class="">
@@ -16,7 +16,7 @@
         </div>
         <div
           v-motion-pop
-          class="relative bg-neutral-300/25 backdrop-blur-md rounded-md p-4"
+          class="relative bg-neutral-300/25 backdrop-blur-md rounded-md p-4 overflow-y-auto"
           :class="props.class"
         >
           <slot></slot>
@@ -29,6 +29,7 @@
 <script setup lang="ts">
 import { LuX } from '@kalimahapps/vue-icons'
 import { watch, onBeforeUnmount } from 'vue'
+import { useBodyScrollLock } from '@/composables/useBodyScrollLock'
 
 type Props = {
   show: boolean
@@ -38,6 +39,8 @@ const props = defineProps<Props>()
 
 const emit = defineEmits(['close'])
 
+const { unlockBodyScroll, lockBodyScroll } = useBodyScrollLock()
+
 const close = () => {
   emit('close')
 }
@@ -46,22 +49,30 @@ const handleKeydown = (e: KeyboardEvent) => {
   if (e.key === 'Escape') close()
 }
 
+const addListeners = () => {
+  window.addEventListener('keydown', handleKeydown)
+}
+
+const removeListeners = () => {
+  window.removeEventListener('keydown', handleKeydown)
+}
+
 watch(
   () => props.show,
   (newVal) => {
     if (newVal) {
-      document.body.classList.add('overflow-hidden')
-      window.addEventListener('keydown', handleKeydown)
+      lockBodyScroll()
+      addListeners()
     } else {
-      document.body.classList.remove('overflow-hidden')
-      window.removeEventListener('keydown', handleKeydown)
+      unlockBodyScroll()
+      removeListeners()
     }
   },
   { immediate: true }, // Handle initial mount state
 )
 
 onBeforeUnmount(() => {
-  document.body.classList.remove('overflow-hidden')
-  window.removeEventListener('keydown', handleKeydown)
+  unlockBodyScroll()
+  removeListeners()
 })
 </script>
