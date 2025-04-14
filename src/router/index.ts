6a430b1f9ade3eeb975fb47db0fb0f435b66aee0
Muhammad Arfan <arfan2173@gmail.com>
2025-04-14 21:18:40 +0700

    updated routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', component: import('@/views/AboutView.vue') },
+    { path: '/builds', component: import('@/views/BuildsView.vue'), meta: { title: 'Builds' } },
     { path: '/certs', component: import('@/views/CertsView.vue'), meta: { title: 'Certs' } },
     { path: '/test', component: import('@/views/TestView.vue') },
   ],
