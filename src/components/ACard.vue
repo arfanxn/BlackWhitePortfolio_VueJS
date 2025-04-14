bad9073a6871966192cc09a15700b0e76780e935
Muhammad Arfan <arfan2173@gmail.com>
2025-04-14 15:47:58 +0700

    created reusable card component

---
+++ src/components/ACard.vue
@@ -0,0 +1,11 @@
+<template>
+  <article
+    class="relative size-full flex flex-col rounded-md bg-black border border-neutral-300 hover:border-white p-4"
+  >
+    <slot></slot>
+  </article>
+</template>
+
+<script setup lang="ts">
+//
+</script>
