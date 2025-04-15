8d0b835c597c351e23069327472f77e50127c000
Muhammad Arfan <arfan2173@gmail.com>
2025-04-15 18:03:26 +0700

    updated ADateRange

---
+++ src/components/ADateRange.vue
@@ -0,0 +1,31 @@
+<template>
+  <p class="inline-flex gap-x-1 font-firacode text-neutral-300">
+    <time>{{ startStr }}</time>
+    <span>-</span>
+    <time>{{ endStr }}</time>
+  </p>
+</template>
+
+<script setup lang="ts">
+import moment from 'moment'
+import { computed } from 'vue'
+
+type Props = {
+  start: Date | string | null
+  end: Date | string | null
+  format?: string
+}
+const props = withDefaults(defineProps<Props>(), {
+  format: 'MM/YYYY',
+})
+
+const startStr = computed(() => {
+  const startM = moment(props.start)
+  return startM.isValid() ? startM.format(props.format) : props.start
+})
+
+const endStr = computed(() => {
+  const endM = moment(props.end)
+  return endM.isValid() ? endM.format(props.format) : props.end
+})
+</script>
