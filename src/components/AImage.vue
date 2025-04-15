231ac5ece830c20467883f5bb9f258bc4c5f7b25
Muhammad Arfan <arfan2173@gmail.com>
2025-04-15 16:48:27 +0700

    updated image component

--- src/components/AImage.vue
+++ src/components/AImage.vue
@@ -7,7 +7,7 @@
       class="max-w-full object-contain"
       :class="[props.class]"
     />
-    <div class="absolute inset-0" :class="[layerClass]"></div>
+    <div class="absolute inset-0" :class="[overlayClass]"></div>
     <div
       v-if="isLoading"
       class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
@@ -26,12 +26,12 @@ const props = defineProps<{
   alt?: string
   containerClass?: string
   class?: string
-  layerClass?: string
+  overlayClass?: string
 }>()
 
-const layerClass = computed(() =>
-  props.layerClass
-    ? `${props.layerClass} group-hover/image:bg-transparent transition-colors duration-300`
+const overlayClass = computed(() =>
+  props.overlayClass
+    ? `${props.overlayClass} group-hover/image:bg-transparent transition-colors duration-300`
     : ``,
 )
 
