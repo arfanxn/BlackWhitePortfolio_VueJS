5cd2d8179bdbf52081b6fcd042d0cc47c38c2410
Muhammad Arfan <arfan2173@gmail.com>
2025-04-16 03:44:43 +0700

    renamed file

---
+++ src/components/certs/CertList.vue
@@ -0,0 +1,28 @@
+<template>
+  <section>
+    <ul class="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-8 list-none">
+      <li v-for="certificate in certificateStore.certificates" :key="certificate.name">
+        <CertCard
+          v-motion-slide-visible-once-left
+          :duration="500"
+          :delay="300"
+          :certificate="certificate"
+        />
+      </li>
+    </ul>
+
+    <p
+      v-if="certificateStore.certificates.length === 0"
+      class="font-firacode text-base text-neutral-300"
+    >
+      No certificates found
+    </p>
+  </section>
+</template>
+
+<script setup lang="ts">
+import CertCard from '@/components/certs/CertCard.vue'
+import { useCertificateStore } from '@/stores/useCertificateStore'
+
+const certificateStore = useCertificateStore()
+</script>
