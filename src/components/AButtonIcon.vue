b1f15c3559a807cc6d4d2f6afc8c5d2f90557dac
Muhammad Arfan <arfan2173@gmail.com>
2025-04-16 14:38:47 +0700

    updated button icon

--- src/components/AButtonIcon.vue
+++ src/components/AButtonIcon.vue
@@ -1,15 +1,25 @@
 <template>
   <AButton class="group relative p-2! rounded-full! overflow-hidden hover:bg-neutral-300/25">
-    <component :is="icon" class="text-xl text-neutral-300" :class="[iconClass]" />
+    <AIconTooltipped
+      v-if="tooltipLabel"
+      :icon="icon"
+      :iconClass="iconClass"
+      :tooltipClass="tooltipClass"
+      :tooltipLabel="tooltipLabel"
+    />
+    <component v-else :is="icon" class="text-xl text-neutral-300" :class="[iconClass]" />
   </AButton>
 </template>
 
 <script setup lang="ts">
 import AButton from '@/components/AButton.vue'
 import { type Component } from 'vue'
+import AIconTooltipped from '@/components/AIconTooltipped.vue'
 
 defineProps<{
   icon: Component
   iconClass?: string
+  tooltipClass?: string
+  tooltipLabel?: string
 }>()
 </script>
