9b0a6e28708cd67849789ba81a4c8cab407ccac5
Muhammad Arfan <arfan2173@gmail.com>
2025-04-16 14:43:11 +0700

    created build query components

---
+++ src/components/builds/BuildQuery.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="flex flex-col gap-4 md:gap-4">
+      <AFieldIconed
+        v-model="buildQuery.keyword"
+        @onInput="buildStore.queryBuilds"
+        :icon="LuSearch"
+        placeholder="Search builds"
+        class="w-full md:w-[50%]"
+      />
+
+      <div class="flex flex-row flex-wrap items-center gap-4 md:gap-4">
+        <BuildQueryFilterStack />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import AFieldIconed from '@/components/AFieldIconed.vue'
+import BuildQueryFilterStack from '@/components/builds/BuildQueryFilterStack.vue'
+import { useBuildStore } from '@/stores/useBuildStore'
+import { LuSearch } from '@kalimahapps/vue-icons'
+import { storeToRefs } from 'pinia'
+
+const buildStore = useBuildStore()
+const { buildQuery } = storeToRefs(buildStore)
+</script>
---
+++ src/components/builds/BuildQueryFilterStack.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="relative">
+    <AButtonOutlined @click="toggleDropdown">
+      <span>
+        <span>{{ buildQuery.filters.stacks.length > 1 ? 'Stacks' : 'Stack' }}</span>
+        <span v-if="buildQuery.filters.stacks.length > 0">:</span>
+      </span>
+      <ul class="inline-flex gap-x-2">
+        <li v-for="(stack, index) in buildQuery.filters.stacks" :key="index">
+          <AIconTooltipped iconClass="text-xl" :icon="stack.icon" :tooltipLabel="stack.name" />
+        </li>
+      </ul>
+      <LuChevronDown class="text-xl" />
+    </AButtonOutlined>
+
+    <ADropdownSelect
+      @close="closeDropdown"
+      @optionsSelected="
+        (selectedOptions: Stack[]) => {
+          buildQuery.filters.stacks = selectedOptions ?? []
+          buildStore.queryBuilds()
+        }
+      "
+      :isOpen="isDropdownOpen"
+      :options="stacks"
+      :maxSelectCount="3"
+      class="min-w-58 md:min-w-58 p-2!"
+      selectClass="flex flex-row flex-wrap gap-1"
+    >
+      <template v-slot:option="{ option: stack, isSelected }">
+        <AButtonIcon
+          :icon="stack.icon"
+          iconClass="text-2xl!"
+          class="rounded-md!"
+          :class="[isSelected ? 'bg-neutral-300/25' : '']"
+        />
+      </template>
+    </ADropdownSelect>
+  </div>
+</template>
+
+<script setup lang="ts">
+import AIconTooltipped from '@/components/AIconTooltipped.vue'
+import AButtonOutlined from '@/components/AButtonOutlined.vue'
+import ADropdownSelect from '@/components/ADropdownSelect.vue'
+import { useDropdown } from '@/composables/useDropdown'
+import { LuChevronDown } from '@kalimahapps/vue-icons'
+import { storeToRefs } from 'pinia'
+import { useBuildStore } from '@/stores/useBuildStore'
+import { builds as origin_builds, type Build } from '@/constants/buildConstants'
+import { type Stack } from '@/constants/stackConstants'
+import AButtonIcon from '../AButtonIcon.vue'
+
+const buildStore = useBuildStore()
+const { buildQuery } = storeToRefs(buildStore)
+const stacks: Stack[] = Array.from(
+  new Set(origin_builds.flatMap((build: Build) => build.stacks)),
+).sort((a, b) => a.name.localeCompare(b.name))
+
+const { isDropdownOpen, toggleDropdown, closeDropdown } = useDropdown()
+</script>
