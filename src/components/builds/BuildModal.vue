0e279241f8c65116ae9b74f34383e557c0c7d302
Muhammad Arfan <arfan2173@gmail.com>
2025-04-18 07:12:09 +0700

    added vertical spacing and change color

--- src/components/builds/BuildModal.vue
+++ src/components/builds/BuildModal.vue
@@ -1,67 +1,68 @@
 <template>
-  <AModal :isOpen="isOpen" modalClass="font-firacode text-neutral-300">
+  <AModal :isOpen="isOpen" modalClass="space-y-4">
     <!-- Image Section -->
     <figure class="relative w-full rounded-md overflow-hidden">
       <AImage
         :src="build.imageUrls[0]"
         :alt="`${build.title} build image`"
-        imgClass="max-w-full min-h-24 aspect-auto object-contain"
+        figure-class="rounded-md! overflow-hidden!"
+        overlay-class="rouned-md! overflow-hidden!"
+        imgClass="max-w-full min-h-24 aspect-auto object-contain rounded-md! overflow-hidden!"
       />
       <div
-        class="absolute inset-x-0 bottom-0 bg-neutral-300/25 backdrop-blur flex flex-row items-center justify-end p-1 border-t border-neutral-300/25"
+        class="absolute inset-x-0 bottom-0 bg-black/25 backdrop-blur flex flex-row items-center justify-end p-1 border-t border-neutral-300/25"
       >
         <AButtonIcon
           @click="openImagePreviewModal"
           :icon="LuEye"
-          iconClass="text-white! md:text-2xl! "
+          class="hover:bg-white/25!"
+          iconClass="text-white! md:text-2xl!"
         />
       </div>
     </figure>
 
     <!-- Project Details -->
-    <section class="space-y-4">
-      <header class="space-y-2">
-        <h2 class="text-2xl font-bold text-white">
-          {{ build.title }}
-        </h2>
+    <header class="space-y-2">
+      <h2 class="text-2xl font-firacode font-bold text-white">
+        {{ build.title }}
+      </h2>
 
-        <p class="text-base leading-relaxed">
-          {{ build.description }}
-        </p>
-      </header>
+      <p class="font-firacode text-base text-neutral-300 leading-relaxed">
+        {{ build.description }}
+      </p>
+    </header>
 
-      <!-- Tech Stacks -->
-      <section>
-        <ul class="flex flex-row flex-wrap items-baseline gap-2">
-          <li v-for="stack in build.stacks" :key="stack.name">
-            <AIconTooltipped :icon="stack.icon" :tooltipLabel="stack.name" iconClass="text-2xl" />
-          </li>
-        </ul>
-      </section>
+    <!-- Tech Stacks -->
+    <section>
+      <ul class="flex flex-row flex-wrap items-baseline gap-2">
+        <li v-for="stack in build.stacks" :key="stack.name">
+          <AIconTooltipped :icon="stack.icon" :tooltipLabel="stack.name" iconClass="text-2xl" />
+        </li>
+      </ul>
+    </section>
 
-      <!-- Timeline Section -->
-      <section class="space-y-1">
-        <h3 class="text-sm font-firacode font-semibold text-white">Timeline</h3>
-        <p class="text-sm font-firacode">
-          <time>Started {{ formattedStartedAt }} </time>
-          <span v-if="build.completedAt" class="block">
-            <time>Completed {{ formattedCompletedAt }} ({{ developmentDuration }} days) </time>
-          </span>
-          <span v-else class="block">Currently in development</span>
-        </p>
-      </section>
+    <!-- Timeline Section -->
+    <section class="space-y-1">
+      <h3 class="font-firacode font-semibold text-sm text-white">Timeline</h3>
+      <p class="font-firacode text-sm text-neutral-300">
+        <time>Started {{ formattedStartedAt }} </time>
+        <span v-if="build.completedAt" class="block">
+          <time> Completed {{ formattedCompletedAt }} ({{ formattedDevelopmentDuration }}) </time>
+        </span>
+        <span v-else class="block">Currently in development</span>
+      </p>
+    </section>
 
-      <!-- Links Section -->
-      <section class="space-y-2">
-        <h3 class="text-sm font-firacode font-semibold text-white">Links</h3>
+    <!-- Links Section -->
+    <section class="space-y-2">
+      <h3 class="font-firacode font-semibold text-sm text-white">Links</h3>
 
-        <ul class="space-y-1">
-          <!-- Links -->
-          <li v-for="url in build.urls" :key="url">
-            <BuildLink :url="url" />
-          </li>
-        </ul>
-      </section>
+      <ul class="space-y-1">
+        <!-- Links -->
+        <li v-for="url in build.urls" :key="url">
+          <BuildLink :url="url" />
+        </li>
+      </ul>
     </section>
 
     <AImagePreviewModal
@@ -105,5 +106,10 @@ const developmentDuration = computed(() =>
   props.build.completedAt
     ? Math.abs(moment(props.build.startedAt).diff(props.build.completedAt, 'days'))
     : 0,
+)
+const formattedDevelopmentDuration = computed(() =>
+  developmentDuration.value > 1
+    ? `${developmentDuration.value} days`
+    : `${developmentDuration.value} day`,
 )
 </script>
