0b600ae2e4d2c6e14c191826fb3c52c8d03e1dc1
Muhammad Arfan <arfan2173@gmail.com>
2025-04-18 07:49:26 +0700

    make  items centered

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -2,9 +2,9 @@
 <template>
   <header class="fixed top-0 left-0 right-0 z-50 backdrop-blur bg-black/50 w-full">
     <nav
-      class="layout-container mx-auto flex flex-row items-baseline justify-between py-6 md:py-6 layout-horizontal-padding"
+      class="layout-container mx-auto flex flex-row items-center justify-between py-6 md:py-6 layout-horizontal-padding"
     >
-      <ALogo imgClass="max-w-6 md:max-w-8" />
+      <ALogo figureClass="bg-red-500!" imgClass="max-w-6 md:max-w-8" />
 
       <ul class="flex gap-x-4" role="navigation">
         <li v-for="navigation in navigations" :key="navigation.url">
