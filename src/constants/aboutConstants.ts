317f5d61fdfc80a8df3c78af69fc193dae0dd471
Muhammad Arfan <arfan2173@gmail.com>
2025-04-23 09:46:26 +0700

    fix space

--- src/constants/aboutConstants.ts
+++ src/constants/aboutConstants.ts
@@ -1,18 +1,18 @@
 export const shortBiography = {
   first: `<span>Backend Developer and Web 3 Enthusiast from Jakarta.</span>`,
-  last: `<span>Interested in Golang, Rust, and Solidity.</span>`,
+  last: `&nbsp;<span>Interested in Golang, Rust, and Solidity.</span>`,
 }
 
 export const fullBiography = `
 <p>
-I started my coding journey in 2021 during my second year of high school, dedicating 6 to 8 hours a day to learning programming. I explored various fields like frontend, mobile development, backend, AI, DevOps, and Web3. After completing several projects, I realized that backend development and Web3 are where my true passion lies, and I’ve been focusing on these ever since.
+A backend developer and Web3 enthusiast, I began my coding journey in 2021, during my second year of high school, where I dedicated 6 to 8 hours each day to learning programming by self-teaching. I explored various fields like frontend, mobile, backend, AI, DevOps, and Web3. After completing several projects, I realized that backend development and Web3 are where my true passion lies, and I’ve been focusing on these ever since.
+</p>
 
 <p>
-In addition to coding, I’m passionate about philosophy, psychology, and economics, which shape how I approach problem-solving. Communication science is a recent interest, particularly its role in financial markets and crypto investments. Outside of tech, I’m a cat lover and enjoy model photography, both as a photographer and a model. You can check out my work on Instagram: @arfanxn.
+In addition to coding, I’m passionate about philosophy, psychology, and economics, which shape how I approach problem-solving. Communication science is a recent interest, particularly its role in financial markets and crypto investments. Outside of tech, I’m a cat lover and enjoy model photography, both as a photographer and a model. You can check out my work on Instagram:  <a class="underline" href="https://www.instagram.com/arfanxn/" target="_blank">@arfanxn</a>.
 </p>
 
 <p>
 I’m always eager to learn and grow, whether it's in technology or other areas that spark my curiosity.
 </p>
-
 `
